97e213391bf8c9c26664cee084c680d9cf7dbf72
amrutha2129 <s.vas.amrutha@gmail.com>
2019-06-01 18:59:07 +1000

    Created hCard preview elements

--- src/components/hCardPreviewContainer/hCardPreview/index.jsx
+++ src/components/hCardPreviewContainer/hCardPreview/index.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { Card, CardBody, CardHeader } from "reactstrap";
 import "./style.css";
 
-export default class HCardFormPreview extends React.PureComponent {
+export default class HCardPreview extends React.PureComponent {
   render() {
     return (
       <div className="cardContainer">
         <Card>
-          <CardHeader />
-          <CardBody />
+          <CardHeader className="cardHeader" />
+          <CardBody>
+            <p className="cardItems">EMAIL</p>
+            <p className="cardItems">PHONE</p>
+            <p className="cardItems">ADDRESS</p>
+            <p className="cardItems"> </p>
+            <p className="cardItems">POSTCODE</p>
+          </CardBody>
         </Card>
       </div>
     );
